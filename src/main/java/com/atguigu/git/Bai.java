2b9f307ce833e96f6a31bb148f757175da748b3e
baiyuwei678 <1713150472@qq.com>
2024-05-13 21:05:05 +0800

    aaaa4

---
+++ src/main/java/com/atguigu/git/Bai.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+public class Bai {
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+
+    }
+}
